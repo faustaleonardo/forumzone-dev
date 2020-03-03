7a55d9354a6f6db5633aacd7831c3577acf60fdb
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-03-03 17:51:28 +0700

    fix bugs

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,9 +1,9 @@
-const errorReducer = (state = {}, action) => {
+const errorReducer = (state = '', action) => {
   const { payload } = action;
 
   switch (action.type) {
     case 'get_error':
-      return payload;
+      return payload || false;
     case 'clear_error':
       return payload;
     default:
